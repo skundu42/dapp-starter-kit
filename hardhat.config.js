fcf7acfa1ecaaa79387b3e118af64117a091aeed
skundu42 <skundu_be19@thapar.edu>
2022-01-31 13:59:37 +0530

    polygon deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
 
+const key= process.env.PRIVATE_KEY;
+const nodekey= process.env.ALCHEMY_KEY;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.7.3",
-};
+
+ module.exports = {
+   defaultNetwork: "mumbai",
+   networks: {
+     hardhat: {
+     },
+     mumbai: {
+       url: `https://polygon-mumbai.g.alchemy.com/v2/${nodekey}`,
+       accounts: [key]
+     }
+   },
+   solidity: {
+     version: "0.7.3",
+     settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+     }
+   },
+ }
